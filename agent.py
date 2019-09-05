e43bb75691c5fca948e77960d6020925182b776b
Harald Wilbertz <Harald.Wilbertz@gmx.de>
2019-09-05 17:01:42 +0200

    Soft update method added.

--- agent.py
+++ agent.py
@@ -3,16 +3,16 @@ import random
 import numpy as np
 import torch
 import torch.optim as optim
+import torch.nn.functional as F
 
-import experience_buffer.py
+from .experience_buffer import ExperienceBuffer
 
-
-BUFFER_SIZE = int(1e5)  # replay buffer size
+BUFFER_SIZE = 100000    # replay buffer size
 BATCH_SIZE = 64         # mini batch size
 GAMMA = 0.99            # discount factor
-TAU = 1e-3              # for soft update of target parameters
-LR = 5e-4               # learning rate
-UPDATE_EVERY = 4        # how often to update the network
+TAU = 1e-3              # Soft update of target parameters
+LEARN_RATE = 5e-4       # learning rate
+UPDATE_EVERY = 5        # how often to update the network
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -37,7 +37,7 @@ class Agent:
         # DQ-Network
         self.qnetwork_local = dqnetwork(state_size, action_size, seed).to(device)
         self.qnetwork_target = dqnetwork(state_size, action_size, seed).to(device)
-        self.optimizer = optim.Adam(self.qnetwork_local.parameters(), lr=LR)
+        self.optimizer = optim.Adam(self.qnetwork_local.parameters(), lr=LEARN_RATE)
 
         # Replay memory
         self.memory = ExperienceBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, seed)
@@ -49,7 +49,7 @@ class Agent:
 
     def step(self, state, action, reward, next_state, done):
         # Save experience in replay memory
-        self.memory.add(state, action, reward, next_state, done)
+        self.memory.append(state, action, reward, next_state, done)
 
         # Learn every UPDATE_EVERY time steps.
         self.t_step = (self.t_step + 1) % UPDATE_EVERY
@@ -78,3 +78,51 @@ class Agent:
             return np.argmax(action_values.cpu().data.numpy())
         else:
             return random.choice(np.arange(self.action_size))
+
+    def learn(self, experiences, gamma):
+        """Update value parameters using given batch of experience tuples.
+        Params
+        ======
+            experiences (Tuple[torch.Variable]): tuple of (s, a, r, s', done) tuples
+            gamma (float): discount factor
+        """
+        states, actions, rewards, next_states, dones = experiences
+
+        # Get max predicted Q values (for next states) from target model
+        if self.update_type == 'dqn':
+            Q_targets_next = self.qnetwork_target(next_states).detach().max(1)[0].unsqueeze(1)
+
+        elif self.update_type == 'double_dqn':
+            best_local_actions = self.qnetwork_local(states).max(1)[1].unsqueeze(1)
+            double_dqn_targets = self.qnetwork_target(next_states)
+            Q_targets_next = torch.gather(double_dqn_targets, 1, best_local_actions)
+
+        # Compute Q targets for current states
+        Q_targets = rewards + (gamma * Q_targets_next * (1 - dones))
+
+        # Get expected Q values from local model
+        Q_expected = self.qnetwork_local(states).gather(1, actions)
+
+        # Compute loss
+        loss = F.mse_loss(Q_expected, Q_targets)
+        # Minimize the loss
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        # ------------------- update target network ------------------- #
+        self.soft_update(self.qnetwork_local, self.qnetwork_target, TAU)
+
+    def soft_update(self, local_model, target_model, tau):
+        """Soft update model parameters.
+        θ_target = τ*θ_local + (1 - τ)*θ_target
+        Params
+        ======
+            local_model (PyTorch model): weights will be copied from
+            target_model (PyTorch model): weights will be copied to
+            tau (float): interpolation parameter
+        """
+        for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
+            target_param.data.copy_(tau * local_param.data + (1.0 - tau) * target_param.data)
+
+
