5bcf784a65dd8c98131e39e972e361b034e778b6
Harald Wilbertz <Harald.Wilbertz@gmx.de>
2019-09-05 14:45:44 +0200

    Additional methods for agent.

--- agent.py
+++ agent.py
@@ -1,6 +1,80 @@
+
+import random
+import numpy as np
+import torch
+import torch.optim as optim
+
+import experience_buffer.py
+
+
+BUFFER_SIZE = int(1e5)  # replay buffer size
+BATCH_SIZE = 64         # mini batch size
+GAMMA = 0.99            # discount factor
+TAU = 1e-3              # for soft update of target parameters
+LR = 5e-4               # learning rate
+UPDATE_EVERY = 4        # how often to update the network
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
 class Agent:
     def __init__(self, dqnetwork, state_size, action_size, update_type, seed):
-        pass
+        """Initialize an Agent object.
+
+        Params
+        ======
+            qnetwork (torch.nn.Module): model to use as the function approximation
+            state_size (int): dimension of each state
+            action_size (int): dimension of each action
+            update_type (str): 'dqn' or 'double-dqn'
+            seed (int): random seed
+        """
+        self.state_size = state_size
+        self.action_size = action_size
+        self.seed = random.seed(seed)
+        self.update_type = update_type
+
+        # DQ-Network
+        self.qnetwork_local = dqnetwork(state_size, action_size, seed).to(device)
+        self.qnetwork_target = dqnetwork(state_size, action_size, seed).to(device)
+        self.optimizer = optim.Adam(self.qnetwork_local.parameters(), lr=LR)
+
+        # Replay memory
+        self.memory = ExperienceBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, seed)
+        # Initialize time step (for updating every UPDATE_EVERY steps)
+        self.t_step = 0
 
     def _reset(self):
         pass
+
+    def step(self, state, action, reward, next_state, done):
+        # Save experience in replay memory
+        self.memory.add(state, action, reward, next_state, done)
+
+        # Learn every UPDATE_EVERY time steps.
+        self.t_step = (self.t_step + 1) % UPDATE_EVERY
+        if self.t_step == 0:
+            # If enough samples are available in memory, get random subset and learn
+            if len(self.memory) > BATCH_SIZE:
+                experiences = self.memory.sample()
+                self.learn(experiences, GAMMA)
+
+    def act(self, state, eps=0.):
+        """Returns actions for given state as per current policy.
+
+        Params
+        ======
+            state (array_like): current state
+            eps (float): epsilon, for epsilon-greedy action selection
+        """
+        state = torch.from_numpy(state).float().unsqueeze(0).to(device)
+        self.qnetwork_local.eval()
+        with torch.no_grad():
+            action_values = self.qnetwork_local(state)
+        self.qnetwork_local.train()
+
+        # Epsilon-greedy action selection
+        if random.random() > eps:
+            return np.argmax(action_values.cpu().data.numpy())
+        else:
+            return random.choice(np.arange(self.action_size))
